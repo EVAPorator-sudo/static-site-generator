85eefddd9d366aa25db05826be80dde34f0edb98
EVAPorator-sudo <osxar.jeavons@gmail.com>
2025-01-27 19:57:18 +0000

    Added HTMLNode and test passed succesfully

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode():
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        string_so_far = ""
+        for key in self.props:
+            string_so_far =+ f"{key}={self.props[key]} "
+        string_so_far.removesuffix(" ")
+        return string_so_far
+    
+    def __repr__(self):
+        print(f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})")
---
+++ src/test_htmlnode.py
@@ -0,0 +1,18 @@
+from htmlnode import HTMLNode
+import unittest
+
+class TestHTMLNode():
+    def test_eq(self):
+        node = HTMLNode("h1", "this is a test node", None, {"href": "https://www.google.com"})
+        node2 = HTMLNode("h1", "this is a test node", None, {"href": "https://www.google.com"})
+        node3 = HTMLNode("h1", "this is another test node",["node1", "node2"] , {"href": "https://www.google.com"})
+        node4 = HTMLNode("a", "this is a test node", None, {"targt": "https://www.bing.com"})
+        self.assertEqual(node, node2)
+        self.assertEqual(node.__repr__(), node2.__repr__())
+        self.assertNotEqual(node, node3)
+        self.assertNotEqual(node.__repr__(), node3.__repr__())
+        self.assertNotEqual(node4.__repr__(), node3.__repr__())
+        self.assertNotEqual(node4.__repr__(), "")
+
+if __name__ == "__main__":
+    unittest.main()
