d16d4e69aaaa83e0004a1e6a97325a5fd733753b
EVAPorator-sudo <osxar.jeavons@gmail.com>
2025-01-29 20:58:38 +0000

    Added separation by delimiter and passes all tests successfully

---
+++ src/delimiter.py
@@ -0,0 +1,25 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            text = node.text
+            if text.count(delimiter) %2 != 0:
+                raise Exception("unmatched delimiter")
+            split_text = text.split(delimiter)
+            type = TextType.TEXT
+            if text[0] == delimiter:
+                type = node.type
+            for string in split_text:
+                new_nodes.append(TextNode(string, type))
+                if type == text_type:
+                    type = TextType.TEXT
+                else:
+                    type = text_type
+    return new_nodes
+
+
+         
