3b174c2284e65ac29f6694f26adfe053944f0702
EVAPorator-sudo <osxar.jeavons@gmail.com>
2025-03-02 15:51:55 +0000

    template not loading fix

--- src/pagegenerate.py
+++ src/pagegenerate.py
@@ -25,6 +25,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     print(title)
     template = template.replace("Title", title)
     template = template.replace("Content", content)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     directory_gen(dest_path)
     with open(dest_path, 'w') as dest_file:
         dest_file.write(template)
